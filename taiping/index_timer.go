cc0e4b90d96cd366e25907ee14c200f5a23c4d2e
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-11-07 15:46:37 +0900

    リファクタリング

--- taiping/index_timer.go
+++ taiping/index_timer.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "bufio"
-    "flag"
-    "fmt"
-    "io"
-    "os"
-    "time"
-    "math/rand"
+    "bufio"     // buffered「データ転送」をやるためのもの
+    "flag"      // コマンドラインのフラグを解析
+    "fmt"       // 文字列の入出力
+    "io"        // ioパッケージ インターフェース
+    "os"        // osパッケージ
+    "time"      // タイマー
+    "math/rand" //ランダム
 )
 
 // 配列をシャッフルする
@@ -24,19 +24,18 @@ var t int
 
 func init() {
     //オプションで制限時間をできる
-    flag.IntVar(&t, "t", 1, "制限時間(分)")
+    flag.IntVar(&t, "t", 1, "制限時間")
     flag.Parse()
 }
 
 func main() {
     var (
-        ch_rcv    = myinput(os.Stdin)
+        ch_rcv    = input(os.Stdin) 
         tm        = time.After(time.Duration(t) * time.Minute)
         words     = []string{ "raccoon", "dog", "wild boar", "rabbit", "cow", "horse", "wolf", "hippopotamus", "kangaroo", "fox", "giraffe", "bear", "koala", "bat", "gorilla", "rhinoceros", "monkey", "deer", "zebra", "jaguar", "polar bear", "skunk", "elephant", "raccoon dog", "animal", "reindeer", "rat", "tiger", "cat", "mouse", "buffalo", "hamster", "panda", "sheep", "leopard", "pig", "mole", "goat", "lion", "camel", "squirrel", "donkey", "Crocodile", "lion is very cool!", "this is a dog", "I love donkey", "do you like animal?", "Amphibians", "Butterfly", "excellentswimmer is long language" }
         score     = 0
     )
     fmt.Println()
-
     shuffle(words);
     fmt.Println("タイピングゲームを始めます。制限時間は", t, "分。1語1点、", len(words), "点満点")
     //送信用チャネル
@@ -64,36 +63,40 @@ func main() {
         
     }
     fmt.Println("あなたの点数:", score, "点 / ", len(words), " 点")
-    if score <= 10 {
-      fmt.Println("判定 F")
-    } else if score <= 15 && score > 10 {
-      fmt.Println("判定 E")
-    } else if score <= 20 && score > 15 {
-      fmt.Println("判定 D")
-    } else if score <= 25 && score > 20 {
-      fmt.Println("判定 C")
-    } else if score <= 30 && score > 25 {
-      fmt.Println("判定 B")
-    } else if score <= 35 && score > 30 {
-      fmt.Println("判定 A")
-    } else if score <= 40 && score > 35 {
-      fmt.Println("判定 S")
-    } else if score <= 45 && score > 40 {
-      fmt.Println("判定 SS")
-    } else if score > 45 {
-      fmt.Println("判定 SSS")
+    n := score
+    switch {
+      case n <= 10:
+        fmt.Println("判定 F")
+      case n <= 15 && n > 10:
+        fmt.Println("判定 E")
+      case n <= 20 && n > 15:
+        fmt.Println("判定 D")
+      case n <= 25 && n > 20:
+        fmt.Println("判定 C")
+      case n <= 30 && n > 25:
+        fmt.Println("判定 B")
+      case n <= 35 && n > 30:
+        fmt.Println("判定 A")
+      case n <= 40 && n > 35:
+        fmt.Println("判定 S")
+      case n <= 45 && n > 40:
+        fmt.Println("判定 SS")
+      case n > 45:
+        fmt.Println("判定 SSS")
+      default:
+        fmt.Println("判定 F")
     }
 }
 
-func myinput(r io.Reader) <-chan string {
-    // サブgo ルーチン
-    // 標準入力から受け取った文字列を標準出力へ出力する
-    ch1 := make(chan string)
+// 非同期処理
+// バイト列を読み出すためのインターフェース
+func input(Stdin io.Reader) <-chan string {
+    channel := make(chan string)
     go func() {
-        s := bufio.NewScanner(r)
-        for s.Scan() {
-            ch1 <- s.Text()
+        strings := bufio.NewScanner(Stdin)
+        if strings.Scan() {
+            channel <- strings.Text()
         }
     }()
-    return ch1
+    return channel
 }
